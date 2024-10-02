f76d87362a90959bba554d7bafe590e09869eaf8
jaqsyadam <157209550+jaqsyadam@users.noreply.github.com>
2024-10-02 18:33:37 +0500

    Create DriverManager.java

---
+++ DriverManager.java
@@ -0,0 +1,24 @@
+package patterns.singleton;
+
+public class DriverManager {
+    private static volatile DriverManager instance;
+
+    private DriverManager() {
+    }
+
+    public static DriverManager getInstance() {
+        if (instance == null) {
+            synchronized (DriverManager.class) {
+                if (instance == null) {
+                    instance = new DriverManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String findNearestDriver(String location) {
+        System.out.println("Nearest driver found: " + "Driver1");
+        return "Driver1";
+    }
+}
