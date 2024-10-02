ec0232363f9f4df4afa9c38089403b82322b6e9a
jaqsyadam <157209550+jaqsyadam@users.noreply.github.com>
2024-10-02 18:39:54 +0500

    Create Taxi.java

---
+++ Taxi.java
@@ -0,0 +1,13 @@
+package patterns.factory_method;
+
+public abstract class Taxi {
+    private String driverName;
+    private String license;
+
+    public Taxi(String driverName, String license) {
+        this.driverName = driverName;
+        this.license = license;
+    }
+
+    public abstract void takePassenger(String passenger);
+}
